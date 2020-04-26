e70be52825204f29bc210c1e75ac30fcec8c6509
Steve <steve.clifton@outlook.com>
2020-04-26 15:17:48 +1200

    Update tokens position

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,11 +4,12 @@
     <div class="item">
         <a href="/">My Weather</a>
     </div>
-    <div class="item">
-        <a href="/tokens">Tokens</a>
-    </div>
 
     @auth
+        <div class="item">
+            <a href="/tokens">Tokens</a>
+        </div>
+
         <div class="item pull-right">
             <a href="/logout">Logout</a>
         </div>
